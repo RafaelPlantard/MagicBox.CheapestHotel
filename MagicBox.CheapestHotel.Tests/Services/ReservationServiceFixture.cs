0cb9a6c5a6c0611e2ef5d1655b29b150e9d7b05e
Rafael da Silva Ferreira <rafael_yami@hotmail.com>
2015-12-21 20:27:48 -0200

    New changes on tests. Started tests on reservation service fixture.

--- MagicBox.CheapestHotel.Tests/Services/ReservationServiceFixture.cs
+++ MagicBox.CheapestHotel.Tests/Services/ReservationServiceFixture.cs
@@ -1,8 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using MagicBox.CheapestHotel.Entities;
+using MagicBox.CheapestHotel.Models;
 using MagicBox.CheapestHotel.Service;
 using MagicBox.UWP.Services;
 using MagicBox.UWP.Tests.Interfaces;
@@ -26,11 +25,71 @@ namespace MagicBox.CheapestHotel.Tests.Services
             _reservationService = new ReservationService(_fileReaderService);
         }
 
+        /// <summary>
+        /// Verifies whether the customer input generate from a text line is correct.
+        /// </summary>
+        [TestMethod]
+        public void VerifyConvertToCustomerInputFromTextLine()
+        {
+            var mockedValues = new[]
+            {
+                "Regular: 16Mar2009(mon), 17Mar2009(tues), 18Mar2009(wed)",
+                "Regular: 20Mar2009(fri), 21Mar2009(sat), 22Mar2009(sun)",
+                "Rewards: 26Mar2009(thur), 27Mar2009(fri), 28Mar2009(sat)"
+            };
+
+            var expectedValues = new[]
+            {
+                new CustomerInput
+                {
+                    Customer = CustomerType.Regular,
+                    DatesToReserve = new List<DateTime>
+                    {
+                        new DateTime(2009, 03, 16),
+                        new DateTime(2009, 03, 17),
+                        new DateTime(2009, 03, 18),
+                    }
+                },
+
+                new CustomerInput
+                {
+                    Customer = CustomerType.Regular,
+                    DatesToReserve = new List<DateTime>
+                    {
+                        new DateTime(2009, 03, 20),
+                        new DateTime(2009, 03, 21),
+                        new DateTime(2009, 03, 22),
+                    }
+                },
+
+                new CustomerInput
+                {
+                    Customer = CustomerType.Rewards,
+                    DatesToReserve = new List<DateTime>
+                    {
+                        new DateTime(2009, 03, 26),
+                        new DateTime(2009, 03, 27),
+                        new DateTime(2009, 03, 28),
+                    }
+                },
+            };
+
+            for (var i = 0; i < mockedValues.Length; i++)
+            {
+                var actual = _reservationService.ConvertToCustomerInputFromTextLine(mockedValues[i]);
+
+                System.Diagnostics.Debug.WriteLine("here");
+
+                Assert.AreEqual(expectedValues[i], actual);
+            }
+        }
+
         [TestMethod]
         public void VerifyInitialization()
         {
             Assert.IsNotNull(_fileReaderService);
             Assert.IsNotNull(_reservationService);
+            Assert.IsNotNull(_reservationService.AvailableHotels);
         }
     }
 }
