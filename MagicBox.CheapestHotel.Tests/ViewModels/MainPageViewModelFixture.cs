54e4dfc74972c34e4f797db1babde233f2257040
Rafael da Silva Ferreira <rafael_yami@hotmail.com>
2015-12-24 01:13:06 -0200

    Added a new project for tests.

---
+++ MagicBox.CheapestHotel.Tests/ViewModels/MainPageViewModelFixture.cs
@@ -0,0 +1,38 @@
+﻿using MagicBox.CheapestHotel.Service;
+using MagicBox.CheapestHotel.ViewModels;
+using MagicBox.UWP.Interfaces;
+using MagicBox.UWP.Services;
+using Microsoft.VisualStudio.TestPlatform.UnitTestFramework;
+
+namespace MagicBox.CheapestHotel.Tests.ViewModels
+{
+    /// <summary>
+    /// Set of method used to quality assurance on the view model for the Main page.
+    /// </summary>
+    [TestClass]
+    public sealed class MainPageViewModelFixture : ITestable
+    {
+        private IAlertMessageService _alertMessageService;
+        private IFileReaderService _fileReaderService;
+        private IMainPageViewModel _mainViewModel;
+        private IReservationService _reservationService;
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            _alertMessageService = new AlertMessageService();
+            _fileReaderService = new FileReaderService();
+            _reservationService = new ReservationService(_fileReaderService);
+            _mainViewModel = new MainPageViewModel(_alertMessageService, _reservationService);
+        }
+
+        [TestMethod]
+        public void VerifyInitialization()
+        {
+            Assert.IsNotNull(_alertMessageService);
+            Assert.IsNotNull(_fileReaderService);
+            Assert.IsNotNull(_reservationService);
+            Assert.IsNotNull(_mainViewModel);
+        }
+    }
+}
